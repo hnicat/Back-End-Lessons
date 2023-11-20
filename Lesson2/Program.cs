4bab165796d58a79ef1d8c160f8419008137cebd
hnicat <hnicat70@gmail.com>
2023-11-21 01:41:18 +0400

    Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator__Lesson_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ci Ededi Daxil Edin:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 ci Ededi Daxil Edin:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("2 Ededin Cemi");
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
