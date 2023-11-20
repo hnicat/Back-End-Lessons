71a5c026eb110fe1be8a30b3f06306d89dfc27a0
hnicat <hnicat70@gmail.com>
2023-11-20 22:16:37 +0400

    1-lesson

---
+++ Lesson1/Test.cs
@@ -0,0 +1,13 @@
+using System;
+namespace test{
+    public class Program{
+      public static void Main(){
+      Console.WriteLine("Zehmet Olmasa Adnizi Qeyid Edin");
+      string name=Console.ReadLine();
+      Console.WriteLine("Hello "+name);
+      Console.ReadLine();
+}
+
+}
+
+}
